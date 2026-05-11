f2507220b54b312a50b99cbebde6e823a6459338
BrenEstefania <brendahs506@gmail.com>
2026-05-11 10:33:25 -0600

    Actualiza mensaje del backend

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Backend funcionando en Azure 🚀');
+  res.send('Backend funcionando ');
 });
 
 app.listen(3000, () => {
